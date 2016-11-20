e9040dcf887d8a10a7f10837a311c584db67e8ae
aurangzaib <aurangzaibsiddiqui@gmail.com>
2016-11-20 10:28:16 +0100

    starting using XCode

---
+++ revise-cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  revise-cpp
+//
+//  Created by aurangzaib on 11/20/16.
+//  Copyright © 2016 siddiqui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
