1c012d1309210a777bde244f93bc98fe072030b1
siddiqui <aurangzaibsiddiqui@gmail.com>
2016-12-03 10:04:49 +0100

    Update listnride.js

--- listnride.js
+++ listnride.js
@@ -7,7 +7,7 @@
     $(function () {
       $.get("https://api.listnride.com/v2/featured", function (a) {
         a.forEach(function (a) {
-          $(".mdl-grid").append(
+          $("#listnride").append(
             '<div class="mdl-cell mdl-cell--4-col mdl-cell--middle"><div class="lnr-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__media"><img src="' +
             a.image_file_1.image_file_1.small.url +
             '" width="100%" height="80%"></div><div class="mdl-card__supporting-text">' + a.brand + ", " +
