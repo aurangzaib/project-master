e681147385f0bf6e0d392aca398471dd1ef7637f
siddiqui <aurangzaibsiddiqui@gmail.com>
2016-12-03 11:37:14 +0100

    Update listnride.js

--- listnride.js
+++ listnride.js
@@ -1,11 +1,13 @@
 $(function () {
   $.get("https://api.listnride.com/v2/featured", function (a) {
-    a.forEach(function (a) {
-      $("#listnride").append(
-        '<div class="mdl-cell mdl-cell--4-col mdl-cell--middle"><div class="lnr-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__media"><img src="' +
-        a.image_file_1.image_file_1.small.url +
-        '" width="100%" height="80%"></div><div class="mdl-card__supporting-text">' + a.brand + ", " +
-        a.category + "</div></div></div>");
+    $.get("https://code.getmdl.io/1.2.1/material.indigo-pink.min.css", function () {
+      a.forEach(function (a) {
+        $("#listnride").append(
+          '<div class="mdl-cell mdl-cell--4-col mdl-cell--middle"><div class="lnr-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__media"><img src="' +
+          a.image_file_1.image_file_1.small.url +
+          '" width="100%" height="80%"></div><div class="mdl-card__supporting-text">' + a.brand + ", " +
+          a.category + "</div></div></div>");
+      });
     });
   });
 });
