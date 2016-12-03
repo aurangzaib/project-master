3dbd7809151b38bedfcc3cb4f39f836f6f3144ea
siddiqui <aurangzaibsiddiqui@gmail.com>
2016-12-03 12:13:56 +0100

    Update listnride.js

--- listnride.js
+++ listnride.js
@@ -1,12 +1,15 @@
 $(function () {
-  $.get("https://api.listnride.com/v2/featured", function (a) {
-    var listnride = $("#listnride"); 
-    a.forEach(function (a) {
-      listnride.append(
-        '<div class="mdl-cell mdl-cell--4-col mdl-cell--middle"><div class="lnr-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__media"><img src="' +
-        a.image_file_1.image_file_1.small.url +
-        '" width="100%" height="80%"></div><div class="mdl-card__supporting-text">' + a.brand + ", " +
-        a.category + "</div></div></div>");
+  $.get("https://rawgit.com/aurangzaib/master-project/master/listnride.css", function (listnrideCss) {
+    $.get("https://api.listnride.com/v2/featured", function (a) {
+      var listnride = $("#listnride");
+      a.forEach(function (a) {
+        componentHandler.upgradeElement(listnride);
+        listnride.append(
+          '<div class="mdl-cell mdl-cell--4-col mdl-cell--middle"><div class="lnr-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__media"><img src="' +
+          a.image_file_1.image_file_1.small.url +
+          '" width="100%" height="80%"></div><div class="mdl-card__supporting-text">' + a.brand + ", " +
+          a.category + "</div></div></div>");
+      });
     });
   });
 });
