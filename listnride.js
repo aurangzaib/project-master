dbd86d73f619b71b25808f362e01da18514242a9
siddiqui <aurangzaibsiddiqui@gmail.com>
2016-12-03 11:12:37 +0100

    Update listnride.js

--- listnride.js
+++ listnride.js
@@ -1,19 +1,11 @@
-// this script has nothing to do w/ master project. its just testing script for my company work.
-
-// this script will render the bikes of listnride in any site.
-// the site has to include this js file in their html.
-// and add `<div>  
-
-// removed css from here
 $(function () {
-    $.get("https://api.listnride.com/v2/featured", function (a) {
-      a.forEach(function (a) {
-        $("#listnride").append(
-          '<div class="mdl-cell mdl-cell--4-col mdl-cell--middle"><div class="lnr-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__media"><img src="' +
-          a.image_file_1.image_file_1.small.url +
-          '" width="100%" height="80%"></div><div class="mdl-card__supporting-text">' + a.brand + ", " +
-          a.category + "</div></div></div>");
-      });
+  $.get("https://api.listnride.com/v2/featured", function (a) {
+    a.forEach(function (a) {
+      $("#listnride").append(
+        '<div class="mdl-cell mdl-cell--4-col mdl-cell--middle"><div class="lnr-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__media"><img src="' +
+        a.image_file_1.image_file_1.small.url +
+        '" width="100%" height="80%"></div><div class="mdl-card__supporting-text">' + a.brand + ", " +
+        a.category + "</div></div></div>");
     });
   });
 });
