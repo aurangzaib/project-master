2926cf42b6712c1232bdbc3d132285e2496c8c92
siddiqui <aurangzaibsiddiqui@gmail.com>
2016-12-03 10:20:03 +0100

    Update listnride.js

--- listnride.js
+++ listnride.js
@@ -1,6 +1,3 @@
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.blue_grey-blue.min.css">
-<link rel="stylesheet" href="https://rawgit.com/aurangzaib/master-project/master/listnride.css">
 // this script has nothing to do w/ master project. its just testing script for my company work.
 
     // this script will render the bikes of listnride in any site.
@@ -8,13 +5,15 @@
     // and add `<div>
     
     $(function () {
-      $.get("https://api.listnride.com/v2/featured", function (a) {
-        a.forEach(function (a) {
-          $("#listnride").append(
-            '<div class="mdl-cell mdl-cell--4-col mdl-cell--middle"><div class="lnr-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__media"><img src="' +
-            a.image_file_1.image_file_1.small.url +
-            '" width="100%" height="80%"></div><div class="mdl-card__supporting-text">' + a.brand + ", " +
-            a.category + "</div></div></div>")
-        })
-      })
+       $.get("https://rawgit.com/aurangzaib/master-project/master/listnride.css", function(data) {
+             $.get("https://api.listnride.com/v2/featured", function (a) {
+                a.forEach(function (a) {
+                $("#listnride").append(
+                    '<div class="mdl-cell mdl-cell--4-col mdl-cell--middle"><div class="lnr-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__media"><img src="' +
+                    a.image_file_1.image_file_1.small.url +
+                    '" width="100%" height="80%"></div><div class="mdl-card__supporting-text">' + a.brand + ", " +
+                    a.category + "</div></div></div>")
+                });
+            });
+        });
     });
