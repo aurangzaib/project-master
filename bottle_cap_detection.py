487d3b8af04a0dcf319e05f06a69fdd348ff4bc8
siddiqui <siddiqui@siddiqui-spaceship.local>
2017-04-01 11:29:34 +0200

    updated python porting of the code.

--- bottle_cap_detection.py
+++ bottle_cap_detection.py
@@ -27,8 +27,8 @@ class BottleDetection(object):
 
         return thresh
 
-    def apply_hough_transform(self, thresh):
-        self.input_image.copyTo(self.output_image)
+    def apply_hough_transform(self, thresh, with_wait_key):
+        self.output_image = self.input_image.copy()
         hough_lines = cv.HoughLines(
             thresh,
             1,
@@ -37,28 +37,35 @@ class BottleDetection(object):
             0,
             0
         )
+        for hough_line in hough_lines:
+            for rho, theta in hough_line:
 
-        for line in hough_lines:
-            rho = line[0]
-            theta = line[1]
+                a = np.cos(theta)
+                b = np.sin(theta)
 
-            a = np.cos(theta)
-            b = np.sin(theta)
+                x0 = rho * a
+                y0 = rho * b
 
-            x0 = rho * a
-            y0 = rho * b
+                rows, columns, channels = self.input_image.shape
+                pt1 = {
+                    "x": int(np.round(x0 + int(rows) * (-b))),
+                    "y": int(np.round(y0 + int(rows) * a))
+                }
 
-            pt1 = {
-                "x": np.round(x0 + self.input_image.rows * (-b)),
-                "y": np.round(y0 + self.input_image.rows * (a))
-            }
+                pt2 = {
+                    "x": int(np.round(x0 - int(rows) * (-b))),
+                    "y": int(np.round(x0 - int(rows) * a))
+                }
 
-            pt2 = {
-                "x": np.round(x0 - self.input_image.rows * (-b)),
-                "y": np.round(x0 - self.input_image.rows * (a))
-            }
+                cv.line(self.output_image,
+                        (pt1['x'], pt1['y']),
+                        (pt2['x'], pt2['y']),
+                        (0, 0, 255),
+                        2)
 
-            cv.line(self.output_image, pt1, pt2, (0, 0, 255), 1)
+                cv.imshow("result", self.output_image)
+                if with_wait_key:
+                    cv.waitKey(1)
 
     def apply_probabilistic_hough_transform(self, thresh, with_wait_key):
         hough_lines = cv.HoughLinesP(thresh, 1, 180 * (np.pi / 180), 10, 10, 10)
@@ -75,7 +82,16 @@ class BottleDetection(object):
 
         cv.imshow("result", self.output_image)
         if with_wait_key:
-            cv.waitKey()
+            cv.waitKey(1)
+
+    @staticmethod
+    def get_region_of_interest(reference_image, x, y, width, height):
+        # not working correctly right now
+        rec = reference_image[x:y, x+width:y+height]
+        reference_image[x:y, x+width:y+width] = rec
+        print rec
+        cv.imshow("rect mask", rec)
+        cv.waitKey(1)
 
     def compute_results(self, with_wait_key=True):
         thresh = self.apply_filters(self.input_image)
--- master_project.py
+++ master_project.py
@@ -3,5 +3,5 @@ from video_frames import VideoFrames
 prj_dir = "/Users/siddiqui/Documents/Projects/master-project/meetings/"
 input_image = cv.imread(prj_dir + "meeting-10/horizontal-perpendicular/52.bmp")
 
-VideoFrames.operation_on_video_frames(prj_dir + "meeting-10/meeting-10.mp4", True, 0,0)
+VideoFrames.operation_on_video_frames(prj_dir + "meeting-10/meeting-10.mp4", True, 0, 0)
 cv.waitKey()
